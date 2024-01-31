6242e0b6a7803ca4dc7db2c53d4cbf6fef174d01
Javier Lizarraga <55290682+javierlga@users.noreply.github.com>
2024-01-31 09:02:20 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "release_version" {
   value       = helm_release.this.metadata.0.version
   description = "A SemVer 2 conformant version string of the chart."
 }
+
+output "manifest" {
+  value       = try(helm_release.this.manifest, null)
+  description = "The manifest of the release deployed."
+}
